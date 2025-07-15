e046eca1642f10104fe55218b47c5d6f4d3e3242
aayusha-nepa <aayush@nepawholesale.com>
2025-07-15 07:37:48 +0545

    News cleaned

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 import pandas as pd
 import pickle as pk
+import re,string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+nltk.download('stopwords')
+words = stopwords.words("english")
+stemmer = PorterStemmer()
+
 
 file = open('LogisticRegression.pickle', 'rb')    
 model = pk.load(file)
@@ -13,5 +22,8 @@ if st.button('Submit'):
     df = pd.DataFrame(
         {'text':[news]}
     )
+    
+    df['text'] = list(map(lambda x: " ".join([i for i in x.lower().split() if i not in words]), df['text']))
+    df['text'] = df['text'].apply(lambda x: " ".join([stemmer.stem(i) for i in re.sub("[^a-zA-Z]", " ", x).split() ]).lower())
     result = model.predict(df['text'])
     st.write(result)
